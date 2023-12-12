1d38534e7a09c04a74bf44083a3139e5ab8bfb80
guillaumewolfe <guillaumewolfe@gmail.com>
2023-12-12 09:39:46 -0500

    Adding settings and options windows

--- include/states/statesIncludes.h
+++
@@ -1,5 +0,0 @@
-#include <states/BaseState.h>
-#include <states/LoadingState.h>
-#include <states/MenuState.h>
-#include <states/SimulationState.h>
-#include <states/EndState.h>
--- src/engine/CreationTools/StateCreator.cpp
+++
@@ -1,7 +0,0 @@
-#include "engine/CreationTools/StateCreator.h"
-#include "engine/CreationTools/CreatorManager.h"
-
-StateCreator::StateCreator(RenderContext* renderContext, CreatorManager* manager)
-    : m_renderContext(renderContext), m_manager(manager) {
-    // Autre code d'initialisation si nécessaire
-}
